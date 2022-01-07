27749d22db514640b28d32f8d1eaeec0c08ed7a6
miuhuru <zermoun.yasmine@gmail.com>
2022-01-07 15:20:18 +0100

    add function that takes users input and stores the book objects into an array

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+let myLibrary = [];
+
+function Book (title, author, pages) {
+    this.title = title 
+    this.author = author 
+    this.pages = pages 
+}
+
+function addBookToLibrary() {
+    console.log("addbook has been fired!")    
+    const createBook = Object.create(Book);
+    createBook.title = document.querySelector(".titleInput").value
+    createBook.author = document.querySelector(".authorInput").value
+    createBook.pages = document.querySelector(".pagesInput").value
+    myLibrary.push(createBook)
+    console.table(myLibrary)
+}
+
+const addbutton = document.querySelector(".addbutton")
+
+addbutton.addEventListener("click", addBookToLibrary)
