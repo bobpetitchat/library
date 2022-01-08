a5619595712465cc6a22794bd15c3f9577d2f618
miuhuru <zermoun.yasmine@gmail.com>
2022-01-08 15:00:17 +0100

    added card

--- script.js
+++ script.js
@@ -7,19 +7,37 @@ function Book (title, author, pages) {
     this.author = author 
     this.pages = pages 
 }
-
-function addBookToLibrary() {
-    console.log("addbook has been fired!")    
-    const createBook = Object.create(Book);
-    createBook.title = document.querySelector(".titleInput").value
-    createBook.author = document.querySelector(".authorInput").value
-    createBook.pages = document.querySelector(".pagesInput").value
-    myLibrary.push(createBook)
-    console.table(myLibrary)
-}
-
 const addbutton = document.querySelector(".addbutton")
-addbutton.addEventListener("click", addBookToLibrary)
+
+function addBookToLibrary() {   
+    addbutton.addEventListener("click", () => {
+        console.log("addbooktolibrary has been fired!");
+        let books = new Book()
+        books.title = document.querySelector(".titleInput").value
+        books.author = document.querySelector(".authorInput").value
+        books.pages = document.querySelector(".pagesInput").value
+        myLibrary.push(books)
+        displayBook()
+        clearEv()
+    }
+    )
+}
+addBookToLibrary()
+    
+function clearEv() {
+    document.querySelector(".titleInput").value = "";
+document.querySelector(".authorInput").value = "";
+    document.querySelector(".pagesInput").value = "";
+}
+const cardTitle = document.querySelector(".cardTitle")
+const cardAuthor = document.querySelector(".cardAuthor")
+const cardPages = document.querySelector(".cardPages")
+
+function displayBook () {
+    cardTitle.innerText = "Title: " + document.querySelector(".titleInput").value
+    cardAuthor.innerText = "Author: " + document.querySelector(".authorInput").value
+    cardPages.innerText = "Number of pages: " + document.querySelector(".pagesInput").value 
+}
 
 const open = document.getElementById("open");
 const close = document.getElementById("close");
@@ -32,3 +50,4 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
 	modal.classList.remove("show");
 });
+
